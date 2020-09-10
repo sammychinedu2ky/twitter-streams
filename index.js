def0fdb3ddefb7744db9496082ec57d60c588d8a
Samson Amaugo <sammychinedu2ky@gmail.com>
2020-09-10 09:33:03 +0100

    init

---
+++ index.js
@@ -0,0 +1,120 @@
+let fetch = require("node-fetch");
+let https = require("https");
+
+let org = [
+  "Microsoft",
+  "MSFTResearch",
+  "Office",
+  "MicrosoftTeams",
+  "AzureDevOps",
+  "MicrosoftIoT",
+  "MSEdgeDev",
+  "onedrive",
+  "Windows",
+  "VisualStudio",
+  "MSFTImagine",
+  "Azure",
+  "reactjs",
+  "nodejs",
+  "graphqlweekly",
+  "graphqlsummit",
+  "apollographql",
+  "prisma",
+  "typescript",
+  "oscafrica",
+  "OpenSourceOrg",
+  "freeCodeCamp",
+  "MySQL",
+  "MongoDB",
+  "Netlify",
+  "vercel",
+  "GatsbyJS",
+  "PostgreSQL",
+];
+
+// Get ids
+fetch("https://api.twitter.com/2/tweets/search/stream/rules", {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${process.env.token}`,
+  },
+})
+  .then((i) => i.json())
+  .then((i) => {
+    if (!i.data) {
+      i.data = [];
+    }
+    let ids = i.data.map((i) => i.id);
+    console.log(ids);
+    //delete rules
+    let val = {
+      delete: {
+        ids: ids,
+      },
+    };
+    fetch("https://api.twitter.com/2/tweets/search/stream/rules", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.token}`,
+      },
+      body: JSON.stringify(val),
+    })
+      .then((i) => i.json())
+      .then((i) => {
+        console.log(i, "sam");
+        //create rules
+        let val = {
+          add: org.map(i=>{
+              return ({
+                  value: `from:${i} -is:retweet`
+              })
+          })
+            
+        };
+        fetch("https://api.twitter.com/2/tweets/search/stream/rules", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.token}`,
+          },
+          body: JSON.stringify(val),
+        })
+          .then((i) => i.json())
+          .then((i) => {
+            console.log(i, "sam");
+            https.get(
+              "https://api.twitter.com/2/tweets/search/stream",
+              {
+                method: "GET",
+                timeout: 20000,
+                headers: {
+                  Authorization: `Bearer ${process.env.token}`,
+                },
+              },
+              (res) => {
+                res.on("data", (d) => {
+                  let res = Buffer.from(d).toString();
+                  try {
+                    console.log(JSON.parse(res));
+                  } catch (error) {
+                    console.log(error);
+                  }
+                });
+                res.on("error", (err) => {
+                  console.log(err);
+                });
+              }
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ readme.md
@@ -0,0 +1,37 @@
+### Twitter streaming setup
+This repository contains the code of what my setup looks like when trying to utilize twitter API to listen for tweets from some tech accounts like:
+
+  -  "Microsoft",
+  - "MSFTResearch",
+  - "Office",
+  - "MicrosoftTeams",
+  - "AzureDevOps",
+  - "MicrosoftIoT",
+  - "MSEdgeDev",
+  - "onedrive",
+  - "Windows",
+  - "VisualStudio",
+  - "MSFTImagine",
+  - "Azure",
+  - "reactjs",
+  - "nodejs",
+  - "graphqlweekly",
+  - "graphqlsummit",
+  - "apollographql",
+  - "prisma",
+  - "typescript",
+  - "oscafrica",
+  - "OpenSourceOrg",
+  - "freeCodeCamp",
+  - "MySQL",
+  - "MongoDB",
+  - "Netlify",
+  - "vercel",
+  - "GatsbyJS",
+  - "PostgreSQL",
+ 
+ ## How it works
+ - It fetches the ids of  available rules
+ - It deletes those rules
+ - It creates rules 
+ - And streaming starts
